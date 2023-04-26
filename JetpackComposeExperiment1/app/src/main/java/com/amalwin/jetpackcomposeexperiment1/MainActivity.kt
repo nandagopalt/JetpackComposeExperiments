14fb6c97ba6f4649f598d3363d289f53575966db
RT Nalan Sarrwin <gopikrish.thiru@gmail.com>
2023-04-26 17:22:48 +0530

    Experimenting Box Layout and its attributes

--- JetpackComposeExperiment1/app/src/main/java/com/amalwin/jetpackcomposeexperiment1/MainActivity.kt
+++ JetpackComposeExperiment1/app/src/main/java/com/amalwin/jetpackcomposeexperiment1/MainActivity.kt
@@ -5,14 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -31,18 +32,8 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colors.background
                 ) {
-                    Column(
-                        modifier = Modifier.background(color = Color.Gray)
-                            .fillMaxHeight()
-                            .border(2.dp, color = Color.Red)
-                            .padding(10.dp),
-                        verticalArrangement = Arrangement.Top
-                    ) {
-                        Greeting(name = "ABC")
-                        Greeting(name = "DEF")
-                        Greeting(name = "GHIJK")
-                    }
-
+                    //BoxLayoutV1("Hi")
+                    BoxLayoutV2()
                 }
             }
         }
@@ -76,6 +67,146 @@ fun GreetingV2(name: String) {
             .border(2.dp, color = Color.Green)
             .padding(10.dp)
     )
+}
+
+@Composable
+fun BoxLayoutV1(name: String) {
+    Box(
+        modifier = Modifier
+            .background(color = Color.Yellow)
+            .size(180.dp, 150.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .background(color = Color.Red)
+                .size(80.dp, 80.dp)
+                .align(Alignment.TopEnd)
+        ) {}
+        Text(
+            text = "Hi $name",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier
+                .background(color = Color.Green)
+                .padding(10.dp)
+                .align(Alignment.BottomStart)
+        )
+    }
+}
+
+@Composable
+fun BoxLayoutV2() {
+    Box(
+        modifier = Modifier
+            .background(color = Color.Yellow)
+            .padding(1.dp)
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "Top Start",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red,
+            modifier = Modifier
+                .background(color = Color.White)
+                .border(2.dp, color = Color.Green)
+                .padding(10.dp)
+                .align(Alignment.TopStart)
+        )
+        Text(
+            text = "Top Center",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red,
+            modifier = Modifier
+                .background(color = Color.White)
+                .border(2.dp, color = Color.Green)
+                .padding(10.dp)
+                .align(Alignment.TopCenter)
+        )
+        Text(
+            text = "Top End",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red,
+            modifier = Modifier
+                .background(color = Color.White)
+                .border(2.dp, color = Color.Green)
+                .padding(10.dp)
+                .align(Alignment.TopEnd)
+        )
+
+        Text(
+            text = "Center Start",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red,
+            modifier = Modifier
+                .background(color = Color.White)
+                .border(2.dp, color = Color.Green)
+                .padding(10.dp)
+                .align(Alignment.CenterStart)
+        )
+        Text(
+            text = "Center Center",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red,
+            modifier = Modifier
+                .background(color = Color.White)
+                .border(2.dp, color = Color.Green)
+                .padding(10.dp)
+                .align(Alignment.Center)
+        )
+        Text(
+            text = "Center End",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red,
+            modifier = Modifier
+                .background(color = Color.White)
+                .border(2.dp, color = Color.Green)
+                .padding(10.dp)
+                .align(Alignment.CenterEnd)
+        )
+        Text(
+            text = "Bottom Start",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red,
+            modifier = Modifier
+                .background(color = Color.White)
+                .border(2.dp, color = Color.Green)
+                .padding(10.dp)
+                .align(Alignment.BottomStart)
+        )
+        Text(
+            text = "Bottom Center",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red,
+            modifier = Modifier
+                .background(color = Color.White)
+                .border(2.dp, color = Color.Green)
+                .padding(10.dp)
+                .align(Alignment.BottomCenter)
+        )
+        Text(
+            text = "Bottom End",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red,
+            modifier = Modifier
+                .background(color = Color.White)
+                .border(2.dp, color = Color.Green)
+                .padding(10.dp)
+                .align(Alignment.BottomEnd)
+        )
+
+
+    }
+
 }
 
 @Preview(showBackground = true)
