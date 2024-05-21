a7944a8fb38505e8ac29c3b6d728168a7cbfcd7d
RT Nalan Sarrwin <gopikrish.thiru@gmail.com>
2024-05-21 11:46:48 +0530

    Practice exercises

--- MyJetpackComposeApplication/app/src/main/java/com/amalwin/myjetpackcomposeapplication/MainActivity.kt
+++ MyJetpackComposeApplication/app/src/main/java/com/amalwin/myjetpackcomposeapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.amalwin.myjetpackcomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -26,8 +27,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.amalwin.myjetpackcomposeapplication.ui.theme.MyJetpackComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,8 +50,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingText(name: String) {
     Text(
-        text = "Welcome $name",
-        modifier = Modifier.fillMaxHeight(0.5f)
+        text = "Welcome $name", modifier = Modifier.fillMaxHeight(0.5f)
     )
 }
 
@@ -59,41 +63,134 @@ fun GreetingTextV2(name: String) {
 fun GreetingButton(name: String = "Hi") {
     Surface(modifier = Modifier.fillMaxSize(), color = Color.DarkGray) {
         Column(
-            modifier = Modifier
-                .fillMaxSize()
+            modifier = Modifier.fillMaxSize()
         ) {
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
+                modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
             ) {
-                Button(
-                    modifier = Modifier
-                        .wrapContentSize(align = Alignment.CenterEnd)
-                        .weight(1f),
+                Button(modifier = Modifier
+                    .wrapContentSize(align = Alignment.CenterEnd)
+                    .weight(1f),
                     onClick = { }) {
                     //GreetingText(name = "Nandagopal")
                     GreetingTextV2(name = "Nalan Sarrwin RT")
                 }
-                Text(text = "Welcome",color = Color.White)
+                Text(text = "Welcome", color = Color.White)
             }
             Row() {
-                Text(text = "Second line aligned left", modifier = Modifier.wrapContentSize(align = Alignment.CenterEnd).weight(1f), color = Color.White)
+                Text(
+                    text = "Second line aligned left",
+                    modifier = Modifier
+                        .wrapContentSize(align = Alignment.CenterEnd)
+                        .weight(1f),
+                    color = Color.White
+                )
                 Spacer(modifier = Modifier.weight(1f))
-                Text(text = "Second line aligned right", modifier = Modifier.wrapContentSize(), color = Color.White)
+                Text(
+                    text = "Second line aligned right",
+                    modifier = Modifier.wrapContentSize(),
+                    color = Color.White
+                )
             }
             Row() {
-                Text(text = "Third line aligned left", modifier = Modifier.wrapContentSize(), color = Color.White)
+                Text(
+                    text = "Third line aligned left",
+                    modifier = Modifier.wrapContentSize(),
+                    color = Color.White
+                )
                 Spacer(modifier = Modifier.weight(1f))
-                Text(text = "Third line aligned right", modifier = Modifier.wrapContentSize(), color = Color.White)
+                Text(
+                    text = "Third line aligned right",
+                    modifier = Modifier.wrapContentSize(),
+                    color = Color.White
+                )
+            }
+            Row() {
+                Text(
+                    text = "Hello Mr.XYZ",
+                    modifier = Modifier
+                        .clickable(onClick = {})
+                        .padding(all = 24.dp)
+                        .width(200.dp)
+                        .height(240.dp),
+                    style = TextStyle(
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        fontFamily = FontFamily.Cursive
+                    )
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                Spacer(modifier = Modifier.weight(1f))
+                Text(
+                    text = "Hello Mr.ABC",
+                    modifier = Modifier
+                        .wrapContentSize(),
+                    style = MaterialTheme.typography.h3,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 18.sp,
+                    fontFamily = FontFamily.Cursive,
+                    color = Color.White
+                )
             }
         }
     }
 }
 
+@Composable()
+fun MainScreen() {
+    Surface(
+        color = Color.DarkGray,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Surface(
+            modifier = Modifier.wrapContentSize(align = Alignment.Center),
+            color = Color.Magenta
+        ) {
+            Text(
+                text = "Wrapped Content",
+                modifier = Modifier.wrapContentSize(),
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.Light
+            )
+
+        }
+
+    }
+}
+
+@Composable
+fun MainScreenV2() {
+    Surface(modifier = Modifier.fillMaxSize(), color = Color.DarkGray) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            HorizontalColoredBar(Color.Red)
+            HorizontalColoredBar(Color.Magenta)
+            HorizontalColoredBar(Color.Cyan)
+            HorizontalColoredBar(Color.Yellow)
+            HorizontalColoredBar(Color.Blue)
+        }
+    }
+}
+
+@Composable
+fun HorizontalColoredBar(color: Color) {
+    Surface(
+        modifier = Modifier
+            .height(600.dp)
+            .width(60.dp), color = color
+    ) { }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-
-    GreetingButton("Android")
-
+    //GreetingButton("Android")
+    //MainScreen()
+    MainScreenV2()
 }
